d68eb6361805306469b4fc79793e58b6ac75de69
Pukyss <AnRogozin@googlemail.com>
2021-08-12 22:58:47 +0100

    a3

--- src/pages/Python.js
+++ src/pages/Python.js
@@ -3,6 +3,7 @@ import {Col, Row, Card, Button, Tabs, Tab} from 'react-bootstrap';
 import './Pages.css';
 import Pdata from "./data/PythonBasics.json";
 import P2data from "./data/PythonBasics2.json";
+import P3data from "./data/PythonBasics3.json";
 function testUnd(t) {
   if (t === undefined) {
     return '';
@@ -31,6 +32,21 @@ const Python = () => {
             </Col>
             <Col xs={6} md={4}>
             {P2data.map(p => {return(
+                <div key={p.id}>
+                  <Card body bg = 'dark' text = 'white'>
+                    <Card.Title>{p.head}</Card.Title>
+                   <p> <Card.Text>{p.code} </Card.Text>
+                    <Card.Text>{p.code2}</Card.Text>
+                    <Card.Text>{p.code3}</Card.Text>
+                    <Card.Text>{p.code4}</Card.Text></p>
+                    <Card.Text className="text-success">{p.exp}</Card.Text>
+                    <Button variant="primary" onClick={() =>  navigator.clipboard.writeText(p.code + testUnd(p.code2) + testUnd(p.code3) + testUnd(p.code4))}>{p.but}</Button>
+                  </Card>
+                </div>
+              )})}
+            </Col>
+            <Col xs={6} md={4}>
+            {P3data.map(p => {return(
                 <div key={p.id}>
                   <Card body bg = 'dark' text = 'white'>
                     <Card.Title>{p.head}</Card.Title>
