e9e29543aa803d99a24303b72f79985f4a849095
AgedDriuha <anrogozin@googlemail.com>
2021-08-15 21:26:01 +0100

    a4

--- src/pages/Python.js
+++ src/pages/Python.js
@@ -4,6 +4,7 @@ import './Pages.css';
 import Pdata from "./data/PythonBasics.json";
 import P2data from "./data/PythonBasics2.json";
 import P3data from "./data/PythonBasics3.json";
+import CLogic from "./data/conditionalLogic.json";
 import { BiCopyAlt } from "react-icons/bi"
 function testUnd(t) {
   if (t === undefined) {
@@ -20,52 +21,53 @@ const Python = () => {
             <Col xs={6} md={4}>
               {Pdata.map(p => {return(
                 <div key={p.id}>
-                  <Card body bg = 'dark' text = 'white'>
-                    <Card.Title>{p.head} 
-                    </Card.Title>
-                    <Card.Text>
-                               <div>{p.code}</div>
-                               <div>{p.code1}</div>
-                               <div>{p.code2}</div>
-                               <div>{p.code3}</div>
-                               <div>{p.code4}</div>
-                    </Card.Text>
-                    <Card.Text className="text-success">
-                                <div>{p.exp}</div>
-                                <div>{p.exp2}</div>
-                    </Card.Text>
-                    <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">{p.but}</Tooltip>}>
-                      <Button variant="primary" onClick={() =>  navigator.clipboard.writeText
-                            (p.code + testUnd(p.code2) + testUnd(p.code3) + testUnd(p.code4))}><BiCopyAlt/>
-                      </Button>
-                    </OverlayTrigger>
-                  </Card>
-                </div>
+                <Card body bg = 'dark' text = 'white'>
+                  <Card.Title>{p.head}</Card.Title>
+                  <Card.Text>
+                             <div>{p.code}</div>
+                             <div>{p.code1}</div>
+                             <div>{p.code2}</div>
+                             <div>{p.code3}</div>
+                             <div>{p.code4}</div>
+                             <div>{p.code5}</div>
+                  </Card.Text>
+                  <Card.Text className="text-success">
+                              <div>{p.exp}</div>
+                              <div>{p.exp2}</div>
+                  </Card.Text>
+                  <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">{p.but}</Tooltip>}>
+                    <Button variant="primary" onClick={() =>  navigator.clipboard.writeText
+                          (p.code + testUnd(p.code2) + testUnd(p.code3) + testUnd(p.code4) + testUnd(p.code5))}><BiCopyAlt/>
+                    </Button>
+                  </OverlayTrigger>
+                </Card>
+              </div>
               )})}
             </Col>
             <Col xs={6} md={4}>
             {P2data.map(p => {return(
                 <div key={p.id}>
-                  <Card body bg = 'dark' text = 'white'>
-                    <Card.Title>{p.head}</Card.Title>
-                    <Card.Text>
-                               <div>{p.code}</div>
-                               <div>{p.code1}</div>
-                               <div>{p.code2}</div>
-                               <div>{p.code3}</div>
-                               <div>{p.code4}</div>
-                    </Card.Text>
-                    <Card.Text className="text-success">
-                                <div>{p.exp}</div>
-                                <div>{p.exp2}</div>
-                    </Card.Text>
-                    <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">{p.but}</Tooltip>}>
-                      <Button variant="primary" onClick={() =>  navigator.clipboard.writeText
-                            (p.code + testUnd(p.code2) + testUnd(p.code3) + testUnd(p.code4))}><BiCopyAlt/>
-                      </Button>
-                    </OverlayTrigger>
-                  </Card>
-                </div>
+                <Card body bg = 'dark' text = 'white'>
+                  <Card.Title>{p.head}</Card.Title>
+                  <Card.Text>
+                             <div>{p.code}</div>
+                             <div>{p.code1}</div>
+                             <div>{p.code2}</div>
+                             <div>{p.code3}</div>
+                             <div>{p.code4}</div>
+                             <div>{p.code5}</div>
+                  </Card.Text>
+                  <Card.Text className="text-success">
+                              <div>{p.exp}</div>
+                              <div>{p.exp2}</div>
+                  </Card.Text>
+                  <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">{p.but}</Tooltip>}>
+                    <Button variant="primary" onClick={() =>  navigator.clipboard.writeText
+                          (p.code + testUnd(p.code2) + testUnd(p.code3) + testUnd(p.code4) + testUnd(p.code5))}><BiCopyAlt/>
+                    </Button>
+                  </OverlayTrigger>
+                </Card>
+              </div>
               )})}
             </Col>
             <Col xs={6} md={4}>
@@ -79,6 +81,7 @@ const Python = () => {
                                <div>{p.code2}</div>
                                <div>{p.code3}</div>
                                <div>{p.code4}</div>
+                               <div>{p.code5}</div>
                     </Card.Text>
                     <Card.Text className="text-success">
                                 <div>{p.exp}</div>
@@ -86,7 +89,7 @@ const Python = () => {
                     </Card.Text>
                     <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">{p.but}</Tooltip>}>
                       <Button variant="primary" onClick={() =>  navigator.clipboard.writeText
-                            (p.code + testUnd(p.code2) + testUnd(p.code3) + testUnd(p.code4))}><BiCopyAlt/>
+                            (p.code + testUnd(p.code2) + testUnd(p.code3) + testUnd(p.code4) + testUnd(p.code5))}><BiCopyAlt/>
                       </Button>
                     </OverlayTrigger>
                   </Card>
@@ -95,7 +98,45 @@ const Python = () => {
             </Col>
           </Row>
         </Tab>
-        <Tab eventKey="2" title="2">        
+        <Tab eventKey="Conditional logic" title="Conditional logic">
+        <Row>
+            <Col xs={6} md={4}>
+              {CLogic.map(p => {return(
+                <div key={p.id}>
+                <Card body bg = 'dark' text = 'white'>
+                  <Card.Title>{p.head}</Card.Title>
+                  <Card.Text>
+                             <div>{p.code}</div>
+                             <div>{p.code1}</div>
+                             <div>{p.code2}</div>
+                             <div><span>{"  "}
+                             </span>{p.code3}</div>
+                             <div>{p.code4}</div>
+                             <div>{p.code5}</div>
+                             <div>{p.code6}</div>
+                             <div>{p.code7}</div>
+                             <div>{p.code8}</div>
+                  </Card.Text>
+                  <Card.Text className="text-success">
+                              <div>{p.exp}</div>
+                              <div>{p.exp2}</div>
+                  </Card.Text>
+                  <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">{p.but}</Tooltip>}>
+                    <Button variant="primary" onClick={() =>  navigator.clipboard.writeText
+                          (p.code + testUnd(p.code2)
+                                  + testUnd(p.code3)
+                                  + testUnd(p.code4) 
+                                  + testUnd(p.code5) 
+                                  + testUnd(p.code6) 
+                                  + testUnd(p.code7) 
+                                  + testUnd(p.code8))}><BiCopyAlt/>
+                    </Button>
+                  </OverlayTrigger>
+                </Card>
+              </div>
+              )})}
+            </Col>            
+          </Row>        
         </Tab>
       </Tabs> 
     </div>
