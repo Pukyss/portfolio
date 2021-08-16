05e22a77ba06603f62a13308c8cd902877350e39
AgedDriuha <anrogozin@googlemail.com>
2021-08-16 22:03:47 +0100

    a6

--- src/pages/Python.js
+++ src/pages/Python.js
@@ -5,6 +5,7 @@ import Pdata from "./data/PythonBasics.json";
 import P2data from "./data/PythonBasics2.json";
 import P3data from "./data/PythonBasics3.json";
 import CLogic from "./data/conditionalLogic.json";
+//import CLogic2 from "./data/conditionalLogic2.json";
 import { BiCopyAlt } from "react-icons/bi"
 function testUnd(t) {
   if (t === undefined) {
@@ -102,7 +103,7 @@ const Python = () => {
         <Row>
             <Col xs={6} md={4}>
               {CLogic.map(p => {return(
-                <div key={1}>
+                <div key={p.id}>
                 <Card body bg = 'dark' text = 'white'>
                   <Card.Title>{p.head}</Card.Title>
                   <Card.Text>
@@ -110,31 +111,38 @@ const Python = () => {
                              <div>{p.code1}</div>
                              <div>{p.code2}<p></p></div>
                              <div>{p.code3}</div>
-                             <div className='ml-3'> {p.code4}<p></p></div>
+                             <div>{p.codes1}</div>
+                             <div className='ml-3'>{p.codes2}</div>
+                             <div className='ml-3'> {p.code4}<p></p></div>                             
                              <div>{p.code5}</div>
                              <div className='ml-3'>{p.code6}<p></p></div>
                              <div>{p.code7}</div>
-                             <div className='ml-3'>{p.code8}</div>
+                             <div className='ml-3'>{p.code8}<p></p></div>
+                             <div>{p.code9}</div>
+                             <div className='ml-3'>{p.code10}</div>
+                             
                   </Card.Text>
                   <Card.Text className="text-success">
                               <div>{p.exp}</div>
                               <div>{p.exp2}</div>
+                              <div>{p.exp3}</div>
+                              <div>{p.exp4}</div>
+                              <div>{p.exp5}</div>
+                              <div>{p.exp6}</div>
+                              <div>{p.exp7}</div>
+                              <div>{p.exp8}</div>
                   </Card.Text>
                   <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">{p.but}</Tooltip>}>
                     <Button variant="primary" onClick={() =>  navigator.clipboard.writeText
-                          (p.code + testUnd(p.code2)
-                                  + testUnd(p.code3)
-                                  + testUnd(p.code4) 
-                                  + testUnd(p.code5) 
-                                  + testUnd(p.code6) 
-                                  + testUnd(p.code7) 
-                                  + testUnd(p.code8))}><BiCopyAlt/>
+                                   (testUnd(p.code) + testUnd(p.code2) + testUnd(p.code3) + testUnd(p.codes1) + testUnd(p.codes2) + testUnd(p.code4)
+                                   + testUnd(p.code5) + testUnd(p.code6) + testUnd(p.code7) + testUnd(p.code8) + testUnd(p.code9) + testUnd(p.code10)
+                                  )}><BiCopyAlt/>
                     </Button>
                   </OverlayTrigger>
                 </Card>
               </div>
               )})}
-            </Col>            
+            </Col>                      
           </Row>        
         </Tab>
       </Tabs> 
