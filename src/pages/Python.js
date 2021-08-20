c41ad0f3c80beb19b2257c7a764a67e3c0b295dd
AgedDriuha <anrogozin@googlemail.com>
2021-08-21 00:31:55 +0100

    a3

--- src/pages/Python.js
+++ src/pages/Python.js
@@ -6,6 +6,7 @@ import P2data from "./data/PythonBasics2.json";
 import P3data from "./data/PythonBasics3.json";
 import CLogic from "./data/conditionalLogic.json";
 import CLogic2 from "./data/conditionalLogic2.json";
+import CLogic3 from "./data/conditionalLogic3.json";
 import { BiCopyAlt } from "react-icons/bi"
 
 function testUnd(t) {
@@ -185,6 +186,57 @@ const Python = () => {
                     <Button variant="primary" onClick={() =>  navigator.clipboard.writeText
                                    (testUnd(p.code) + testUnd(p.code2) + testUnd(p.code3) + testUnd(p.codes1) + testUnd(p.codes2) + testUnd(p.codes2a) + testUnd(p.codes2b) + testUnd(p.codes1a1) 
                                    + testUnd(p.code4) + testUnd(p.codes3) + testUnd(p.codes4) + testUnd(p.code5) + testUnd(p.code6) + testUnd(p.code7) + testUnd(p.code8) + testUnd(p.code9) + testUnd(p.code10)
+                                  )}><BiCopyAlt/>
+                    </Button>
+                  </OverlayTrigger>
+                </Card>
+              </div>
+              )})}
+            </Col>
+            <Col xs={6} md={4}>
+              {CLogic3.map(p => {return(
+                <div key={p.id}>
+                <Card body bg = 'dark' text = 'white'>
+                  <Card.Title>{p.head}</Card.Title>
+                  <Card.Text>
+                             <div>{p.code}</div>
+                             <div>{p.code1}</div>
+                             <div>{p.code2}<p></p></div>
+                             <div>{p.code3}</div>
+                             <div>{p.codes1}</div>
+                             <div className='ml-3'>{p.codes2}</div>
+                             <div className='ml-3'>{p.codes2a}</div>
+                             <div className='ml-3'>{p.codes2b}</div> 
+                             <div className='ml-5'>{p.codes2b1}</div>
+                             <div className='ml-5'>{p.codes2b2}<p></p></div>
+                             <div className='ml-3'>{p.codes2c}</div> 
+                             <div className='ml-5'>{p.codes2c1}</div>                               
+                             <div className='ml-3'>{p.code4}<p></p></div>
+                             <div>{p.code5}</div>
+                             <div>{p.code6}</div>                             
+                             <div>{p.code7}</div>
+                             <div className='ml-3'>{p.code8}<p></p></div>
+                             <div>{p.code9}</div>
+                             <div>{p.code10}</div>
+                             <div className='ml-3'>{p.code11}<p></p></div>
+                             <div>{p.code12}</div>
+                             <div className='ml-3'>{p.code13}</div>
+                             
+                  </Card.Text>
+                  <Card.Text className="text-success">
+                              <div>{p.exp}</div>
+                              <div>{p.exp2}</div>
+                              <div>{p.exp3}</div>
+                              <div>{p.exp4}</div>
+                              <div>{p.exp5}</div>
+                              <div>{p.exp6}</div>
+                              <div>{p.exp7}</div>
+                              <div>{p.exp8}</div>
+                  </Card.Text>
+                  <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">{p.but}</Tooltip>}>
+                    <Button variant="primary" onClick={() =>  navigator.clipboard.writeText
+                                   (testUnd(p.code) + testUnd(p.code2) + testUnd(p.code3) + testUnd(p.codes1) + testUnd(p.codes2) + testUnd(p.codes2a) + testUnd(p.codes2b) + testUnd(p.codes2b1) + testUnd(p.codes2b2) + testUnd(p.codes2c) + testUnd(p.codes2c1)
+                                   + testUnd(p.code4) + testUnd(p.code5) + testUnd(p.code6) + testUnd(p.code7) + testUnd(p.code8) + testUnd(p.code9) + testUnd(p.code10) + testUnd(p.code11) + testUnd(p.code12)+ testUnd(p.code12)
                                   )}><BiCopyAlt/>
                     </Button>
                   </OverlayTrigger>
